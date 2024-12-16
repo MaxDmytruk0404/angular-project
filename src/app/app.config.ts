16739c8372f1d6946443273844bc9bbdd0c89ece
MaxDmytruk0404 <rordm279@gmail.com>
2024-12-16 15:39:38 +0200

    optimization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,13 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
-
-
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),  
+  providers: [
+    provideRouter(routes),  
     provideHttpClient(withFetch()),
-    { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    provideClientHydration(),
   ]
 };
